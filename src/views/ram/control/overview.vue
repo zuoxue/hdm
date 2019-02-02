f1f1ebad1aba661dc8b11395c515a79d0f261061
zuoxue <zuoxue88@126.com>
2019-02-02 11:16:25 +0800

    delete overview component

--- src/views/ram/control/overview.vue
+++ src/views/ram/control/overview.vue
@@ -117,10 +117,6 @@
         </el-scrollbar>
       </el-main>
     </el-container>
-    <!-- <useroverlay :title="overlayTitle" :isclose="isclose" :width="width">
-      <component :is=""></component> <!--显示不同的按钮跳转组件-->
-    <!-- </useroverlay>  -->
-
   </div>
 </template>
 
